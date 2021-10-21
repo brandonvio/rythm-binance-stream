92bcf24a77a97386124641bfd06e9d82a93f3616
Brandon Vicedomini <brandonvio@outlook.com>
2021-10-21 14:09:00 -0600

    Build.

--- service/app.py
+++ service/app.py
@@ -1,9 +1,20 @@
+from copy import Error
+import logging
 from unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager import BinanceWebSocketApiManager
 
-ubwa = BinanceWebSocketApiManager(exchange="binance.us")
-ubwa.create_stream(['trade'], ['btcusdt', 'ethusdt', 'ethbtc'], output="UnicornFy")
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+logging.getLogger().addHandler(logging.StreamHandler())
 
-while True:
-    buffer = ubwa.pop_stream_data_from_stream_buffer()
-    if buffer:
-        print(buffer)
+logger.debug("rythm-binance-stream starting.")
+
+try:
+    ubwa = BinanceWebSocketApiManager(exchange="binance.us")
+    ubwa.create_stream(['trade'], ['btcusdt', 'ethusdt', 'ethbtc'], output="UnicornFy")
+
+    while True:
+        buffer = ubwa.pop_stream_data_from_stream_buffer()
+        if buffer:
+            logger.debug(buffer)
+except Error as e:
+    logger.exception(e)
