b05e9d43af51e2de75591eddf97f8c9cab5cd870
Brandon Vicedomini <brandonvio@outlook.com>
2021-10-21 08:55:02 -0600

    delete all

--- app.py
+++
@@ -1,9 +0,0 @@
-from unicorn_binance_websocket_api.unicorn_binance_websocket_api_manager import BinanceWebSocketApiManager
-
-ubwa = BinanceWebSocketApiManager(exchange="binance.us")
-ubwa.create_stream(['trade'], ['btcusdt', 'ethusdt', 'ethbtc'], output="UnicornFy")
-
-while True:
-    buffer = ubwa.pop_stream_data_from_stream_buffer()
-    if buffer:
-        print(buffer)
--- build.sh
+++
@@ -1,1 +0,0 @@
-docker build -t rythm/binance-stream:latest .
