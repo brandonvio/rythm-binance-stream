494dff8fa8086940a3065823bc8fbe91017a7de2
Brandon Vicedomini <brandonvio@outlook.com>
2021-10-23 08:30:05 -0600

    Adjust provisioned capacity.

--- service-nd/app.js
+++ service-nd/app.js
@@ -5,26 +5,54 @@ const documentClient = new AWS.DynamoDB.DocumentClient({
     region: 'us-west-2',
 })
 
-const url = 'wss://stream.binance.com:9443/ws/btcusdt@trade'
+const url =
+    'wss://stream.binance.com:9443/stream?streams=btcusdt@trade/ethusdt@trade'
 const ws = new WS.WebSocket(url)
 
 ws.on('open', function open() {
     console.log('Service started.')
 })
 
+/*
+2021-10-23T08:18:28.087-06:00	E: 1634998708037,
+
+2021-10-23T08:18:28.087-06:00	s: 'BTCUSDT',
+
+2021-10-23T08:18:28.087-06:00	t: 1112113956,
+
+2021-10-23T08:18:28.087-06:00	p: '61224.64000000',
+
+2021-10-23T08:18:28.087-06:00	q: '0.01113000',
+
+2021-10-23T08:18:28.087-06:00	b: 8018672923,
+
+2021-10-23T08:18:28.087-06:00	a: 8018672887,
+
+2021-10-23T08:18:28.087-06:00	T: 1634998708036,
+
+2021-10-23T08:18:28.087-06:00	m: false,
+
+2021-10-23T08:18:28.087-06:00	M: true
+*/
+
 ws.on('message', async function incoming(message) {
     try {
         const msg = JSON.parse(message.toString())
+        const data = msg.data
         var params = {
             TableName: 'rythm-data',
             Item: {
-                pk: msg['s'],
-                sk: msg['T'].toString(),
+                pk: data['s'],
+                sk: data['T'].toString(),
+                price: data['p'],
+                event_time: data['E'],
+                trade_id: data['t'],
+                quantity: data['q'],
+                is_market_maker: data['m'],
             },
         }
         console.log(params)
-        console.log(msg)
-        // const result = await documentClient.putItem(params)
+        const result = await documentClient.putItem(params)
     } catch (error) {
         console.log(error)
     }
